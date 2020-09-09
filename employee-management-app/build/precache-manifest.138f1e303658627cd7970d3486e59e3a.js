be776fcdedc5950704949942a16d2603a173a092
shreeja.shrimayee <1706364@kiit.ac.in>
2020-09-10 05:20:03 +0530

    second commit

---
+++ employee-management-app/build/precache-manifest.138f1e303658627cd7970d3486e59e3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a13c9f632e3596728c6fcf11fe0cd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbd5479d22241714382e",
+    "url": "/static/js/2.0ae25726.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.0ae25726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b48ab042d714c8c7345",
+    "url": "/static/js/main.89128475.chunk.js"
+  },
+  {
+    "revision": "00036e5924dd22024ca0",
+    "url": "/static/js/runtime-main.df73fbd2.js"
+  }
+]);
